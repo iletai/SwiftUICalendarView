56ae99a5862c035001ad97a76cdc187cab91939f
Tài Lê <iletai@hotmail.com>
2023-12-14 10:53:39 +0700

    + Config CI (#37)

--- Sources/CalendarView/Utils/DateFormatter+Extension.swift
+++ Sources/CalendarView/Utils/DateFormatter+Extension.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import SwiftDate
 
 public extension DateFormatter {
     static let monthAndYear: DateFormatter = {
@@ -25,23 +24,5 @@ public extension DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "EEEEE"
         return formatter
-    }
-    
-    static func toLunarDateString(forDate: Date, format: String = "MMdd") -> String {
-        let dateFormat = DateFormatter()
-        dateFormat.locale = Locales.vietnamese.toLocale()
-        dateFormat.setLocalizedDateFormatFromTemplate(format)
-        dateFormat.calendar = Calendar(identifier: .chinese)
-        if dateFormat.calendar.component(.day, from: forDate) == 1 {
-            dateFormat.setLocalizedDateFormatFromTemplate("d/M")
-        }
-        return dateFormat.string(from: forDate)
-    }
-    
-    static func format(_ date: Date, format: String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locales.vietnamese.toLocale()
-        dateFormatter.setLocalizedDateFormatFromTemplate(format)
-        return dateFormatter.string(from: date)
     }
 }
