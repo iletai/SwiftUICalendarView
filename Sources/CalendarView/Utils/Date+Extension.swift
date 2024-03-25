e68342589d00fdbb55f8c921dc3821e8b87c7793
Tài Lê <iletai@hotmail.com>
2024-03-25 22:16:37 +0700

    Add Documentation For View (#55)
    * Add documentation
    
    * Add status auto merge when build pr is passing
    
    * Modify auth by me allow merge
    
    * approved pr

--- Sources/CalendarView/Utils/Date+Extension.swift
+++ Sources/CalendarView/Utils/Date+Extension.swift
@@ -8,7 +8,9 @@
 import Foundation
 import SwiftDate
 
+/// Extension for `Date` providing additional utility methods.
 extension Date {
+    /// Returns the name of the week day for the date.
     public var weekDayName: String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
@@ -16,6 +18,7 @@ extension Date {
         return dateFormatter.string(from: self)
     }
 
+    /// Returns the short name of the week day for the date.
     public var weekDayShortName: String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EE"
@@ -23,6 +26,7 @@ extension Date {
         return dateFormatter.string(from: self)
     }
 
+    /// Returns the day name for the date.
     public var dayName: String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "d"
@@ -30,18 +34,34 @@ extension Date {
         return dateFormatter.string(from: self)
     }
 
+    /// Returns the start of the year for the date in the specified calendar.
+    ///
+    /// - Parameter inCalendar: The calendar to use.
+    /// - Returns: The start of the year for the date.
     public func startOfYear(_ inCalendar: Calendar) -> DateInRegion {
         currentDateInRegion(inCalendar).dateAtStartOf(.year)
     }
 
+    /// Returns the start of the month for the date in the specified calendar.
+    ///
+    /// - Parameter inCalendar: The calendar to use.
+    /// - Returns: The start of the month for the date.
     public func startOfMonth(_ inCalendar: Calendar) -> DateInRegion {
         currentDateInRegion(inCalendar).dateAtStartOf(.month)
     }
 
+    /// Returns the end of the year for the date in the specified calendar.
+    ///
+    /// - Parameter inCalendar: The calendar to use.
+    /// - Returns: The end of the year for the date.
     public func endOfYear(_ inCalendar: Calendar) -> DateInRegion {
         currentDateInRegion(inCalendar).dateAtEndOf(.year)
     }
 
+    /// Returns the current date in the specified calendar.
+    ///
+    /// - Parameter calendar: The calendar to use.
+    /// - Returns: The current date in the specified calendar.
     public func currentDateInRegion(_ calendar: Calendar) -> DateInRegion {
         DateInRegion(self, region: .currentIn(calendar: calendar))
     }
