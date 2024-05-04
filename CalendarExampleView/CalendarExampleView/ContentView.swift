ca6b0cbec00dd09e0a8b1dbb6eeb159fa7d5bce6
iletai <iletai@hotmail.com>
2024-05-04 17:38:09 +0700

    !Refactoring

--- CalendarExampleView/CalendarExampleView/ContentView.swift
+++ CalendarExampleView/CalendarExampleView/ContentView.swift
@@ -21,76 +21,82 @@ struct ContentView: View {
     @State var isHightLightToDay = true
     var body: some View {
         VStack {
-            CalendarView(
-                date: selectedDate
-                , dateView: { date in
-                    VStack {
-                        Text(date.dayName)
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                            .foregroundColor(
-                                Calendar.current.isDateInWeekend(date) ? .red : .black
-                            )
-                    }
-                    .frameInfinity()
-                    .frame(height: 30)
-                    .background(listSelectedDate.contains(date) ? .cyan : .clear)
-                }, headerView: { date in
-                    HStack {
-                        ForEach(date, id: \.self) {
-                            Text($0.weekDayShortName.uppercased())
-                                .font(.footnote)
-                                .fontWeight(.bold)
-                                .foregroundColor(
-                                    Calendar.current.isDateInWeekend($0) ? .red : .black
-                                )
-                                .frame(maxWidth: .infinity)
+            ScrollView {
+                VStack {
+                    CalendarView(
+                        date: selectedDate
+                        , dateView: { date in
+                            VStack {
+                                Text(date.dayName)
+                                    .font(.footnote)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(
+                                        Calendar.current.isDateInWeekend(date) ? .red : .black
+                                    )
+                            }
+                            .frameInfinity()
+                            .frame(height: 30)
+                            .background(listSelectedDate.contains(date) ? .cyan : .clear)
+                        }, headerView: { date in
+                            HStack {
+                                ForEach(date, id: \.self) {
+                                    Text($0.weekDayShortName.uppercased())
+                                        .font(.footnote)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(
+                                            Calendar.current.isDateInWeekend($0) ? .red : .black
+                                        )
+                                        .frame(maxWidth: .infinity)
+                                }
+                            }
+                        }, dateOutView: { date in
+                            VStack {
+                                Text(date.dayName)
+                                    .font(.footnote)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(
+                                        Calendar.current.isDateInWeekend(date) ? .red.opacity(0.4) : .gray
+                                    )
+                            }
+                            .frameInfinity()
+                            .frame(height: 30)
+                            .background(listSelectedDate.contains(date) ? .cyan : .clear)
+                        }
+                    )
+                    .enableHeader(isShowHeader)
+                    .enableDateOut(isShowDateOut)
+                    .firstWeekDay(firstWeekDate)
+                    .calendarLocate(locale: Locales.vietnamese.toLocale())
+                    .enablePinedView([.sectionHeaders, .sectionFooters])
+                    .setViewMode(viewMode)
+                    .rowsSpacing(8)
+                    .columnSpacing(8)
+                    .background(.visible(12, .gray.opacity(0.1)))
+                    .onDraggingEnded { direction, viewMode in
+                        if direction == .forward {
+                            withAnimation(.easeInOut) {
+                                selectedDate = selectedDate.dateAt(
+                                    viewMode == .month ? .nextMonth : .nextWeek
+                                ).date
+                            }
+                        } else {
+                            withAnimation(.easeInOut) {
+                                selectedDate = selectedDate.dateAt(
+                                    viewMode == .month ? .prevMonth : .prevWeek
+                                ).date
+                            }
                         }
                     }
-                }, dateOutView: { date in
-                    VStack {
-                        Text(date.dayName)
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                            .foregroundColor(
-                                Calendar.current.isDateInWeekend(date) ? .red.opacity(0.4) : .gray
-                            )
-                    }
-                    .frameInfinity()
-                    .frame(height: 30)
-                    .background(listSelectedDate.contains(date) ? .cyan : .clear)
+                    .onSelectDate(onSelectedDate)
+                    .enableDivider(isShowDivider)
+                    .enableHighlightToDay(isHightLightToDay)
+                    .marginDefault()
+                    .allowsTightening(true)
+                    Spacer()
+
                 }
-            )
-            .enableHeader(isShowHeader)
-            .enableDateOut(isShowDateOut)
-            .firstWeekDay(firstWeekDate)
-            .calendarLocate(locale: Locales.vietnamese.toLocale())
-            .enablePinedView([.sectionHeaders, .sectionFooters])
-            .setViewMode(viewMode)
-            .rowsSpacing(8)
-            .columnSpacing(8)
-            .background(.visible(12, .gray.opacity(0.1)))
-            .onDraggingEnded { direction, viewMode in
-                if direction == .forward {
-                    withAnimation(.easeInOut) {
-                        selectedDate = selectedDate.dateAt(
-                            viewMode == .month ? .nextMonth : .nextWeek
-                        ).date
-                    }
-                } else {
-                    withAnimation(.easeInOut) {
-                        selectedDate = selectedDate.dateAt(
-                            viewMode == .month ? .prevMonth : .prevWeek
-                        ).date
-                    }
-                }
+                .frame(maxWidth: .infinity)
             }
-            .onSelectDate(onSelectedDate)
-            .enableDivider(isShowDivider)
-            .enableHighlightToDay(isHightLightToDay)
-            .marginDefault()
-            .allowsTightening(true)
-            Spacer()
             VStack {
                 listButtonDemo
                     .padding()
@@ -103,9 +109,7 @@ struct ContentView: View {
                 .frame(width: 300)
                 .padding()
             }
-
         }
-        .frame(maxWidth: .infinity)
     }
 
     var listButtonDemo: some View {
