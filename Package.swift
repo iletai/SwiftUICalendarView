fb761a8db5864312b8fa9676d92f8a34e232bb51
Paweł Skrzypkowski <pawlo133438@gmail.com>
2024-09-23 16:11:50 +0200

    update_swiftdate_from_master_to_newest_release (#78)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/malcommac/SwiftDate.git", branch: "master")
+        .package(url: "https://github.com/malcommac/SwiftDate.git", from: "7.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
